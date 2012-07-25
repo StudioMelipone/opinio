bfbd3fe997f76a4c825a3eb3c9d23c607396c3d3
Romain Durritçague <romain@studiomelipone.eu>
2012-07-25 13:07:24 +0200

    Handle some king of a "theming" system

--- lib/opinio/controllers/helpers.rb
+++ lib/opinio/controllers/helpers.rb
@@ -2,7 +2,7 @@ module Opinio
   module Controllers
     module Helpers
 
-      def comments_for(object, options = {})        
+      def comments_for(object, options = {})
         render_comments(object, options) +
         ( render_comments_form(object, options) unless options[:no_new] ).to_s
       end
@@ -10,11 +10,13 @@ module Opinio
       def render_comments(object, options = {})
         limit = options.delete(:limit) || Opinio.model_name.constantize.default_per_page
         page  = options.delete(:page)  || 1
-        render( :partial => "opinio/comments/comments", :locals => {:comments => object.comments.page(page).limit(limit), :commentable => object, :options => options} )
+        theme = options[:theme] || 'comments'
+        render( :partial => "opinio/#{theme}/comments", :locals => {:comments => object.comments.page(page).limit(limit), :commentable => object, :options => options} )
       end
 
       def render_comments_form(object, options = {})
-        render( :partial => "opinio/comments/new", :locals => {:commentable => object, :options => options} )
+        theme = options.delete(:theme) || 'comments'
+        render( :partial => "opinio/#{theme}/new", :locals => {:commentable => object, :options => options} )
       end
     end
     
